d1ba2e0ffad0d916cb145745bb8ac5914329b5d0
Thomas Dziedzic <gostrc@gmail.com>
2013-06-07 14:15:00 +0000

    create connect_db() and init_db()

--- bfp.py
+++ bfp.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 from flask import Flask, g, request, abort
+from contextlib import closing
 
 DATABASE = '/tmp/bfp.db'
 DEBUG = True
@@ -9,9 +10,18 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 @app.before_request
 def before_request():
-    g.db = sqlite3.connect(app.config['DATABASE'])
+    g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
